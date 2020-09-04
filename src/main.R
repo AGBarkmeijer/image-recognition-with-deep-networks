1f24efa46aef0c723788beb69b0336659d936e09
Mike <mike.hukiewitz@wechselgott.com>
2020-09-04 16:32:12 +0200

    add keras library

--- src/main.R
+++ src/main.R
@@ -1,5 +1,9 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : MLP MNIST-Classifier (Lab Group 17 - MLHVL)
+# Objective : Train a Multi-Layer-Perceptron to classify a set of images,
+# as given by the MNIST handwritten numbers dataset.
 # Created by: mik
 # Created on: 04.09.20
 
+library(keras)
+library(kerasR)
+install_keras()
