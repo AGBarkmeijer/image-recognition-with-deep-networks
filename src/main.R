344bf97d9a2b05ca3c055fb6686b548573415c25
Mike <mike.hukiewitz@wechselgott.com>
2020-09-04 16:42:05 +0200

    add some code from assignment sheet

--- src/main.R
+++ src/main.R
@@ -4,6 +4,17 @@
 # Created by: mik
 # Created on: 04.09.20
 
+# load libraries
 library(keras)
 library(kerasR)
 install_keras()
+
+# load data
+mnist <- dataset_mnist()
+
+# Part: Model definition
+# create model
+model <- keras_model_sequential()
+model %>%
+ layer_dense(units = 256, input_shape = c(784)) %>%
+ layer_dense(units = 10, activation = 'softmax')
